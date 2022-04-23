d68bba69d8cc88089123a4de3ce3f1754da5ef82
Steve C <diceroll123@gmail.com>
2022-04-23 14:06:51 -0400

    Add more NeoFoodClub tests

--- tests/test_bet_functions.py
+++ tests/test_bet_functions.py
@@ -2,7 +2,13 @@ from typing import Tuple
 
 import pytest
 from neofoodclub.errors import InvalidData
-from neofoodclub.neofoodclub import Bets, NeoFoodClub
+from neofoodclub.neofoodclub import Bets, Modifier, NeoFoodClub
+
+
+def test_odds_iter(nfc_with_bet_amount: NeoFoodClub):
+    # bet amounts are NEEDED to make bustproof + guaranteed wins
+    bets = nfc_with_bet_amount.make_bustproof_bets()
+    assert len(list(bets.odds)) == 4
 
 
 def test_guaranteed_win_true(nfc_with_bet_amount: NeoFoodClub):
@@ -31,6 +37,13 @@ def test_make_url(nfc_with_bet_amount: NeoFoodClub):
         bets.make_url(include_domain=False)
         == "/#round=7956&b=eukucjuoycaulucepkyrtukyw&a=CXSCXSCXSCXSCXSCXSCXSCXSCXSCXS"
     )
+
+
+def test_make_url_all_data(nfc: NeoFoodClub, test_max_ter_15_bets: str):
+    new_nfc = NeoFoodClub(nfc.to_dict())
+    new_nfc.modifier = Modifier(cc_perk=True)
+    bets = new_nfc.make_max_ter_bets()
+    assert bets.make_url(all_data=True) == f"https://neofood.club{test_max_ter_15_bets}"
 
 
 def test_short_hash(nfc: NeoFoodClub):
@@ -170,3 +183,41 @@ def test_net_expected_equality_with_amount(
 def test_bets_from_binary_error(nfc: NeoFoodClub, bet_binaries: Tuple[int, ...]):
     with pytest.raises(InvalidData):
         Bets.from_binary(*bet_binaries, nfc=nfc)
+
+
+def test_random_bets(nfc: NeoFoodClub):
+    bets = nfc.make_random_bets()
+    assert len(bets) == 10
+
+
+def test_random_gambit_bets(nfc: NeoFoodClub):
+    bets = nfc.make_gambit_bets(random=True)
+    assert bets.is_gambit is True
+    assert len(bets) == 10
+
+
+def test_random_unit_bets_empty(nfc: NeoFoodClub):
+    bets = nfc.make_units_bets(400000)  # higher than possible
+    assert len(bets) == 0
+
+
+def test_random_unit_bets(nfc: NeoFoodClub):
+    bets = nfc.make_units_bets(20)
+    assert len(bets) == 10
+
+
+def test_bustproof_with_two_positives(nfc: NeoFoodClub):
+    new_nfc = nfc.copy()
+    # setting Buck to 4 sets arena to positive, giving us 2 positives
+    new_nfc.modifier = Modifier(custom_odds={19: 4})
+    bets = new_nfc.make_bustproof_bets()
+    assert bets.is_bustproof is True
+
+
+def test_bustproof_with_three_positives(nfc: NeoFoodClub):
+    new_nfc = nfc.copy()
+    # setting Buck to 4 sets arena to positive, giving us 2 positives
+    # setting Crossblades to 12 sets arena to positive, giving us 3 positives
+    new_nfc.modifier = Modifier(custom_odds={19: 4, 11: 12})
+    bets = new_nfc.make_bustproof_bets()
+    assert bets.is_bustproof is True
--- tests/test_neofoodclub.py
+++ tests/test_neofoodclub.py
@@ -1,8 +1,17 @@
 import datetime
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
+import pytest
 from neofoodclub import NeoFoodClub
+from neofoodclub.errors import InvalidData
 from neofoodclub.neofoodclub import Modifier
+
+
+def test_nfc_reset(nfc: NeoFoodClub):
+    new_nfc = nfc.copy(cache=False)
+
+    # making a bet will run the wrapper's reset
+    assert len(new_nfc.make_bets_from_binaries(0x1)) == 1
 
 
 def test_nfc_copy(nfc: NeoFoodClub, nfc_from_url: NeoFoodClub):
@@ -103,13 +112,55 @@ def test_cc_perk(nfc: NeoFoodClub, nfc_from_url: NeoFoodClub):
     assert new_nfc.max_amount_of_bets == 15
 
 
-def test_mer_winning_odds(nfc: NeoFoodClub, nfc_from_url: NeoFoodClub):
-    # this set wins 24 units
-    bets = nfc.make_bets_from_hash("aukacfukycuulacauutcbukdc")
-    assert nfc.get_win_units(bets) == 24
+@pytest.mark.parametrize(
+    "bet_hash,winnings",
+    [
+        ("aukacfukycuulacauutcbukdc", 24),
+        ("ltqvqwgimhqtvrnywrwvijwnn", 0),
+    ],
+)
+def test_get_win_units(
+    nfc: NeoFoodClub, nfc_from_url: NeoFoodClub, bet_hash: str, winnings: int
+):
+    bets = nfc.make_bets_from_hash(bet_hash)
+    assert nfc.get_win_units(bets) == winnings
 
-    bets = nfc_from_url.make_bets_from_hash("aukacfukycuulacauutcbukdc")
-    assert nfc_from_url.get_win_units(bets) == 24
+    bets_for_url = nfc_from_url.make_bets_from_hash(bet_hash)
+    assert nfc_from_url.get_win_units(bets_for_url) == winnings
+
+
+@pytest.mark.parametrize(
+    "bet_hash,bet_amount,winnings",
+    [
+        ("aukacfukycuulacauutcbukdc", 8000, 192000),
+        ("aukacfukycuulacauutcbukdc", None, 0),
+        ("ltqvqwgimhqtvrnywrwvijwnn", 8000, 0),
+        ("ltqvqwgimhqtvrnywrwvijwnn", None, 0),
+    ],
+)
+def test_get_win_np(
+    nfc: NeoFoodClub,
+    nfc_from_url: NeoFoodClub,
+    bet_hash: str,
+    bet_amount: Optional[int],
+    winnings: int,
+):
+    new_nfc = nfc.copy()
+    new_nfc.bet_amount = bet_amount
+    bets = new_nfc.make_bets_from_hash(bet_hash)
+    assert new_nfc.get_win_np(bets) == winnings
+
+    # this block uses the NFC's bet amount instead of passing the bet amount down to the bets
+    # notice the bet amount is set after the bets are made
+    new_nfc_amount_after_bets = nfc.copy()
+    bets_without_amount = new_nfc_amount_after_bets.make_bets_from_hash(bet_hash)
+    new_nfc_amount_after_bets.bet_amount = bet_amount
+    assert new_nfc_amount_after_bets.get_win_np(bets_without_amount) == winnings
+
+    new_nfc_from_url = nfc_from_url.copy()
+    new_nfc_from_url.bet_amount = bet_amount
+    bets_for_url = new_nfc_from_url.make_bets_from_hash(bet_hash)
+    assert new_nfc_from_url.get_win_np(bets_for_url) == winnings
 
 
 def test_changes(nfc_no_cache: NeoFoodClub, test_round_data: Dict[str, Any]):
@@ -122,3 +173,116 @@ def test_changes(nfc_no_cache: NeoFoodClub, test_round_data: Dict[str, Any]):
     assert nfc_no_cache.changes[0].arena_index == 1
     assert nfc_no_cache.changes[0].pirate.name == "Squire"
     assert nfc_no_cache.changes[0].arena == "Lagoon"
+
+
+def test_get_arena(nfc: NeoFoodClub):
+    assert nfc.get_arena(0).name == "Shipwreck"
+
+
+def test_pirates(nfc: NeoFoodClub):
+    assert nfc.pirates[0][0] == 2
+
+
+def test_changes_equivalence(nfc: NeoFoodClub):
+    changes = list(set(nfc.changes))  # tests __hash__
+
+    assert changes[0] != changes[1]
+    assert list(changes[0]) != list(changes[1])  # tests __iter__
+
+
+def test_change_bet_amount_twice(nfc: NeoFoodClub):
+    # this runs the bet_amount setter code which recalculates
+    # the inner dicts instead of making new ones
+    # this is more for code coverage than testing anything
+    new_nfc = nfc.copy(cache=False)
+
+    assert new_nfc._data_dict == {}
+    new_nfc.bet_amount = 8000
+
+    new_nfc.bet_amount = 5000
+    assert new_nfc._data_dict != {}
+
+
+def test_removed_timestamp(nfc: NeoFoodClub):
+    # timestamp isn't really needed, so if it doesn't exist,
+    # we just return None
+    data = nfc.to_dict()
+
+    data.pop("timestamp")
+
+    new_nfc = NeoFoodClub(data)
+    assert new_nfc.timestamp is None
+
+
+def test_outdated_lock(nfc: NeoFoodClub):
+    # we're not even in the same year anymore
+    # so this should be True
+    assert nfc.is_outdated_lock is True
+
+
+def test_outdated_lock_none(nfc: NeoFoodClub):
+    data = nfc.to_dict()
+
+    # if there's no start attribute, assume it's over
+    data.pop("start")
+
+    new_nfc = NeoFoodClub(data)
+    assert new_nfc.is_outdated_lock is True
+
+
+def test_winners_pirates(nfc: NeoFoodClub):
+    assert len(nfc.winners_pirates) == 5
+
+
+def test_winners_pirates_empty(nfc: NeoFoodClub):
+    data = nfc.to_dict()
+    # monkeypatching in no winners
+    data["winners"] = [0, 0, 0, 0, 0]
+
+    new_nfc = NeoFoodClub(data)
+    assert len(new_nfc.winners_pirates) == 0
+
+
+def test_from_url_exception():
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url("")
+
+
+def test_from_url_with_cc_perk(test_max_ter_15_bets: str):
+    nfc = NeoFoodClub.from_url(test_max_ter_15_bets)
+    assert nfc.modifier.cc_perk == True
+
+
+def test_from_url_without_winners(test_round_url_no_winners: str):
+    nfc = NeoFoodClub.from_url(test_round_url_no_winners)
+    assert nfc.winners == [0, 0, 0, 0, 0]
+
+
+def test_from_url_without_round_exception(test_round_url_no_round: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_no_round)
+
+
+def test_from_url_without_pirates_exception(test_round_url_no_pirates: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_no_pirates)
+
+
+def test_from_url_with_invalid_pirates_exception(test_round_url_invalid_pirates: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_invalid_pirates)
+
+
+def test_from_url_with_no_opening_odds(test_round_url_no_opening_odds: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_no_opening_odds)
+
+
+def test_from_url_with_no_current_odds(test_round_url_no_current_odds: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_no_current_odds)
+
+
+def test_from_url_with_invalid_opening_odds(test_round_url_invalid_opening_odds: str):
+    with pytest.raises(InvalidData):
+        NeoFoodClub.from_url(test_round_url_invalid_opening_odds)
