2cc39a68d5f645336573e4a13e841783a41d65cc
Steve C <diceroll123@gmail.com>
2024-02-24 18:17:04 -0500

    Update math.rs

--- src/math.rs
+++ src/math.rs
@@ -86,20 +86,19 @@ impl Math {
         bet_odds: Vec<u32>,
         probabilities: [[f64; 5]; 5],
     ) -> PyResult<&'a PyTuple> {
-        let elements = neofoodclub::math::build_chance_objects(&bets, &bet_odds, probabilities);
-
-        let py_structs: Vec<PyObject> = elements
-            .into_iter()
-            .map(|chance| {
-                let chance = Chance {
-                    value: chance.value,
-                    probability: chance.probability,
-                    cumulative: chance.cumulative,
-                    tail: chance.tail,
-                };
-                chance.into_py(py)
-            })
-            .collect();
+        let py_structs: Vec<PyObject> =
+            neofoodclub::math::build_chance_objects(&bets, &bet_odds, probabilities)
+                .into_iter()
+                .map(|chance| {
+                    Chance {
+                        value: chance.value,
+                        probability: chance.probability,
+                        cumulative: chance.cumulative,
+                        tail: chance.tail,
+                    }
+                    .into_py(py)
+                })
+                .collect();
 
         Ok(PyTuple::new(py, py_structs))
     }
