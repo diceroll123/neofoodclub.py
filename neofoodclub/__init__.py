d9bdd150ef0e03da3b0cacb6ebf9d2787995ab7e
Steve C <diceroll123@gmail.com>
2024-11-10 14:21:29 -0500

    version bump 1.0.13

--- neofoodclub/__init__.py
+++ neofoodclub/__init__.py
@@ -2,7 +2,7 @@ __title__ = "neofoodclub"
 __author__ = "diceroll123"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present diceroll123"
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 
 from .neofoodclub import *  # noqa
 from .probability_model import *  # noqa
