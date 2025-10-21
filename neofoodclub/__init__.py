8d2ee888f3ec6ccc3491a511bf235f6ed3370aa0
Steve C <diceroll123@gmail.com>
2025-10-20 23:35:42 -0400

    Update version to 1.0.17 in Cargo.toml and pyproject.toml; use dynamic versioning in Python package

--- neofoodclub/__init__.py
+++ neofoodclub/__init__.py
@@ -1,8 +1,10 @@
+from importlib.metadata import version as _version
+
 __title__ = "neofoodclub"
 __author__ = "diceroll123"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present diceroll123"
-__version__ = "1.0.16"
+__version__ = _version("neofoodclub")
 
 from .neofoodclub import *  # noqa
 from .probability_model import *  # noqa
